8ae2b42c6425abdc67b9e4172d62b2a1c5fa27d1
Kate Danilova <ecdanilova@gmail.com>
2022-06-18 21:40:18 -0400

    update info

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".image-container img").forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector(".popup-image").style.display = "block";
+        document.querySelector(".popup-image img").src = image.getAttribute("src");
+    }
+});
+
+document.querySelector(".popup-image span").onclick = () =>{
+    document.querySelector(".popup-image").style.display = "none";
+}
